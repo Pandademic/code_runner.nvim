841890ee086c1a5d6cc9d5c4a4c4f09c718c8b39
Crag <ogeidca49@gmail.com>
2021-10-21 09:37:56 -0500

    prueba de ventana flotante

---
+++ lua/code_runner/float_run.lua
@@ -0,0 +1,44 @@
+local fwin = {}
+local api = vim.api
+local cmd = vim.cmd
+local fn = vim.fn
+
+local M = {}
+
+function M.new_buf()
+    fwin.buf = api.nvim_create_buf(false, true)
+    api.nvim_buf_set_option(fwin.buf, 'filetype', 'CodeRunner')
+end
+
+function M.new_win()
+    fwin.win = api.nvim_open_win(fwin.buf, true, {
+        relative = "editor",
+        style = "minimal",
+        border = "rounded",
+        width = .8,
+        height = .8,
+        col = .5,
+        row = .5
+    })
+    api.nvim_win_set_option(fwin.win, "winhl", "Normal:".. "NONE")
+end
+
+function M.new_term()
+    fwin.term = fn.termopen(os.getenv("SHELL"))
+    cmd("startinsert")
+end
+
+function M.show()
+    if fwin.buf == nil then
+        M.new_buf()
+        M.new_win()
+        M.new_term()
+        api.nvim_chan_send(fwin.term, "clear\n")
+    else
+        M.new_win()
+    end
+    api.nvim_set_current_win(fwin.win)
+    cmd("startinsert")
+end
+
+return M
